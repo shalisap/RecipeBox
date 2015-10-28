3acfbf8619265e1e61c9c71a1b4934d43961b8fe
Shalisa Pattarawuttiwong <shalisa@squareup.com>
2015-10-28 11:05:31 -0700

    First commit

---
+++ app/src/main/java/com/example/shalisa/recipebox/BrowseActivity.java
@@ -0,0 +1,14 @@
+package com.example.shalisa.recipebox;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class BrowseActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browse);
+    }
+}
---
+++ app/src/main/java/com/example/shalisa/recipebox/MainFragment.java
@@ -0,0 +1,31 @@
+package com.example.shalisa.recipebox;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        // When clicking browse button
+        View browseButton = rootView.findViewById(R.id.browseBtn);
+        browseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), BrowseActivity.class);
+                getActivity().startActivity(intent);
+            }
+        });
+
+        return rootView;
+    }
+}
